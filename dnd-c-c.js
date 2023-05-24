faef5301353af4af8bd6561d2adc5238f4969a41
Alan Pring <alanpring@outlook.com>
2023-05-24 12:40:39 +0100

    updates

--- dnd-c-c.js
+++ dnd-c-c.js
@@ -2,7 +2,6 @@
 
 /*
 
-- create a default inventory
 - create a copy of this inventory (non-destructive) and add random items from random starter item array. Need the original array preserved roll character can be rolled again
 
 - character weapon genertor
@@ -49,18 +48,49 @@
 
 //should these be global or block??
 
-const characterInventory = [];
+const characterInventory = { //uses object literals, nest obj lit, nested with arrays
+    clothing: ['hat', 'trousers', 'fine clothes'],
+    armor: {type: 'light', name: 'leather'}, //update based on class? armor type and name
+    weapons: {melee: ['rapier', 'sword'], ranged: ['crossbow']},//update based on class?
+    gear: ['rope', 'blanket', 'tinder box', '12 bolts'], //update ammo count + bolts suffix
+    money: {gold: 10, silver: 0, copper: 0}, //need to display this value with gp suffix on print
+    food: ['apple', 'duck jerky']
+};
+//will need to add additional specific inventory items based on class
+//create a copy of this inventory (non-destructive) and add random items from random starter item array. Need the original array preserved roll character can be rolled again
 
-//objects with multi values
-const defaultInventory = [];
+/*
+//How to access these values
+console.table(characterInventory);
+console.table(characterInventory.clothing);
+console.table(characterInventory.armor);
+console.log(`Armor type is: ${characterInventory.armor.type}`);
+console.log(`Armor type is: ${characterInventory.armor.name}`);
+console.table(characterInventory.weapons);
+console.log(`Melee weapons are: ${characterInventory.weapons.melee}`);
+console.log(`Ranged weapons are: ${characterInventory.weapons.ranged}`);
+console.table(characterInventory.gear);
+console.table(characterInventory.money);
+console.log(`Gold: ${characterInventory.money.gold}`);
+console.table(characterInventory.food);
+console.log(characterInventory.food[1]);
+*/
 
+const characterName = {
+    firstName : ['artimis', 'herald', 'ragnor', 'charlie', 'hunter'],
+    titleNameA : ['deviant', 'bloodied', 'bountiful', 'hateful', 'loving'],
+    titleNameB: ['destroyer', 'killer', 'sailor', 'soldier', 'jester'],
+    lastName : ['bloodthorne', 'clove', 'hope', 'raylor']
+}
+//loop characterName arrays through randomIndexNumber to produce name?
+//e.g. artmis 'the bloddied jester' clove // artmis clove - the bloodied jester
+//take user input?
+//check project name generator for this and refactor
 
 
 const dndRaceImage = [];
 
 const dndClassImage = [];
-
-
 
 
 
@@ -80,44 +110,38 @@ function d20Roll() { //random to generate number, floor to become integer
     document.getElementById("d20-roll-result").innerHTML = d20; //print result to div
 }
 
-function generateRace() {
-    const dndRace = ["Dragonborn", "Dwarf", "Elf", "Gnome", "Half-Elf", "Half-Orc", "Halfling", "Human", "Tiefling"];
-    const raceSelection = dndRace[Math.floor(Math.random() * dndRace.length)];
-    console.log(`Race generated is: ${raceSelection}`);
-    return raceSelection;
-}
 
-function generateCharacter() { //change this to generate individual components instead of all character
-    //console.log("Number of D&D races: " + dndRace.length);
-    //console.log("Number of D&D classes: " + dndClass.length);
+function generateCharacter() { //change this to generate individual components instead of all character?
 
+    //generate random number between 0 and array length - exclusive as array starts at 0
+    function randomIndexNumber(i){
+        return i[Math.floor(Math.random() * i.length)];
+    }
+    
     //race
     const dndRace = ["Dragonborn", "Dwarf", "Elf", "Gnome", "Half-Elf", "Half-Orc", "Halfling", "Human", "Tiefling"];
-    generateRace();
-
-
     //class
     const dndClass = ["Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard"];
-
-    
-    //find length of array
-    //generate random number between 0 and array length - exclusive as array starts at 0
-    //use number to print value from that index
-
-
+    //console.log("Number of D&D races: " + dndRace.length);
+    //console.log("Number of D&D classes: " + dndClass.length);
 
     //          //
     //  OUTPUT  //
     //          //
-    
-    //How can this be refactored? Loop?
-    //Race Selector
-    let raceLength = Math.floor(Math.random() * dndRace.length);
-    //Class Selector
-    let classLength = Math.floor(Math.random() * dndClass.length);
 
-    console.log(`You are a ${dndRace[raceLength]} ${dndClass[classLength]}!`);
-    document.getElementById("character-result").innerHTML = `You are a ${dndRace[raceLength]} ${dndClass[classLength]}!`;
+    raceLength = randomIndexNumber(dndRace);
+    //console.log(`Race using randomNumber function is: ${raceLength}`);
+    classLength = randomIndexNumber(dndClass);
+    //console.log(`Class using randomNumber function is: ${classLength}`);
+
+    //
+    //  PRINT
+    //
+    console.log(`You are a ${raceLength} ${classLength}!`);
+    console.log(`Here is your inventory:`);
+    console.log(characterInventory);
+
+    document.getElementById("character-result").innerHTML = `You are a ${raceLength} ${classLength}!`;
 }
 
 
